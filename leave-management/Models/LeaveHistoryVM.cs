c8c09834b9d3cc0dd0717cee723d2a2025fc9cf3
Mehedi Hasan <me.mehedi@hotmail.com>
2021-01-18 00:09:34 +0600

    AutoMapper Implementation

--- leave-management/Models/LeaveHistoryVM.cs
+++ leave-management/Models/LeaveHistoryVM.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,16 +12,17 @@ namespace leave_management.Models
         public int Id { get; set; }
         public EmployeeVM RequestingEmployee { get; set; }
         public string RequestingEmployeeId { get; set; }
+        [Required]
         public DateTime StartDate { get; set; }
+        [Required]
         public DateTime EndDate { get; set; }
         public DetailsLeaveTypeVM LeaveType { get; set; }
         public int LeaveTypeId { get; set; }
+        public IEnumerable<SelectListItem> LeaveTypes { get; set; }
         public DateTime DateRequested { get; set; }
         public DateTime DateActioneded { get; set; }
         public bool? Approvved { get; set; }
         public EmployeeVM ApprovedBy { get; set; }
         public string ApprovedById { get; set; }
-        public IEnumerable<SelectListItem> Employees { get; set; }
-        public IEnumerable<SelectListItem> LeaveTypes { get; set; }
     }
 }
