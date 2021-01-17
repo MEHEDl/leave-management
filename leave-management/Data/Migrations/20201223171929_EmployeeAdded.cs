a59923afdf1d3db53c5d892bafa077fd5f384366
Mehedi Hasan <me.mehedi@hotmail.com>
2021-01-17 23:28:12 +0600

    Add project files.

---
+++ leave-management/Data/Migrations/20201223171929_EmployeeAdded.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace leave_management.Data.Migrations
+{
+    public partial class EmployeeAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TaxId",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TaxId",
+                table: "AspNetUsers");
+        }
+    }
+}
